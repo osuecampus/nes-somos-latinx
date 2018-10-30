166bf9e2e1cdb41b964dc52e4de4068636816aa9
Austin Spaeth <austinspaeth@msn.com>
2018-10-30 00:24:57 -0700

    feat(Sidebar): Starting to construct dropdown subpages

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -21,7 +21,15 @@ class Tree extends Component {
     return (
       <div className={'contentTree'}>
     
-    { this.props.content[0] ? this.props.content[0].sections.map((plot) => { return <div key={plot.id} className={'unitBlock'}><h3>{plot.title}</h3><div className={'downProgress'}><img src={'./assets/img/icon-arrow.svg'} /></div></div>} ) : ''}
+    { this.props.content[0] ? this.props.content[0].sections.map((plot) => { return (
+        <div key={plot.id}>
+            <div className={'unitBlock'}><h3>{plot.title}</h3><div className={'downProgress'}><img src={'./assets/img/icon-arrow.svg'} /></div></div>
+            {this.props.content[0].sections.map((plex) => { return ( 
+                 plex.id 
+            ) })}
+        </div>
+    )}
+    ) : ''}
 
       </div>
     );
