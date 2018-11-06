6a30c68b35a7ae87ff098d50c102d72d7621cde6
Austin Spaeth <austinspaeth@msn.com>
2018-11-05 21:32:46 -0800

    feat(Content): Made the top section responsive and adjusted line-heights
    fix #30 fix #31 fix #33

--- src/components/Content.js
+++ src/components/Content.js
@@ -24,7 +24,7 @@ class Content extends Component {
             {this.props.content[0] ? 
               (this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage-1] ? 
               (<div onClick={() => this.props.setCurrentPage(this.props.currentPage-1)}><div title={this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage-1].title} className={'pageTitle'}>
-                {this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage-1].title.length > 22 ? this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage-1].title.substr(0,22) + '..': this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage-1].title }
+                {this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage-1].title.length > 16 ? this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage-1].title.substr(0,16) + '..': this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage-1].title }
               </div>
               <div className={'pageDirection'}>Previous Page</div></div>)
               :null)
@@ -36,7 +36,7 @@ class Content extends Component {
             {this.props.content[0] ? 
               (this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage+1] ? 
               (<div onClick={() => this.props.setCurrentPage(this.props.currentPage+1)}><div title={this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage+1].title} className={'pageTitle'}>
-                {this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage+1].title.length > 22 ? this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage+1].title.substr(0,22) + '..': this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage+1].title }
+                {this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage+1].title.length > 16 ? this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage+1].title.substr(0,16) + '..': this.props.content[0].sections[this.props.currentUnit].content[this.props.currentPage+1].title }
               </div>
               <div className={'pageDirection'}>Next Page</div></div>)
               :null)
