dce790299fa1f54db52ce34419e23e49047b88d0
Austin Spaeth <austinspaeth@msn.com>
2018-11-16 16:57:00 -0800

    feat(TextColumns): Made more progress on TextColumns component block
    need to finish off the optional props of the component then it will be done
    
    re #4

--- src/blocks/TextColumns.js
+++ src/blocks/TextColumns.js
@@ -10,7 +10,13 @@ export default class TextColumns extends Component {
   render() {
     return (
       <section className={css(ss.section)}>
-        <p className={css(ss.text)}>{this.props.details.text}</p>
+        {this.props.details.columns.map((block) => 
+          <div className={css(ss.column)} key={block.id}>
+            {block.image ? <img src={block.image} alt={block.imageAlt} title={block.imageAlt} /> : null } 
+            {block.headline ? <h1 className={css(ss.headline)}>{block.headline}</h1> : null }
+            {block.text ? <p className={css(ss.text)}>{block.text}</p> : null }
+          </div>
+        )}
       </section>
     );
   }
@@ -20,10 +26,39 @@ const ss = StyleSheet.create({
   section: {
       width: '100%',
       display:'flex',
-      alignItems:'center',
+      flexDirection:'row',
+      justifyContent:'space-around',
+      alignItems:'flex-start',
+      '@media (max-width: 700px)': {
+        flexDirection: 'column',
+        alignItems:'center',
+      }
   },
   text:{
     width:'100%',
     fontFamily:'Crimson Text',
+    fontSize:'smaller',
+    textAlign:'center'
+  },
+  column:{
+    display:'flex',
+    padding:20,
+    alignItems:'center',
+    justifyContent:'flex-start',
+    flexDirection:'column'
+  },
+  headline:{
+    borderRadius:'50%',
+    background:'#1f60e2',
+    color:'#fff',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    fontSize:28,
+    fontWeight:'900',
+    margin:0,
+    width:60,
+    height:60,
+    marginBottom:15
   }
 });
