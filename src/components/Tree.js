085a89b084f4cefd29fd68a984cc28261433afdb
Austin Spaeth <austinspaeth@msn.com>
2018-11-27 12:15:11 -0800

    fix(Sidebar): When in a mobile view, clicking a page will now make the sidebar disappear
    fix #79

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 // REDUX //
 import { connect } from "react-redux";
-import { loadContent, setCurrentUnit, setCurrentPage } from "../redux/Actions";
+import { loadContent, setCurrentUnit, setCurrentPage, detectDimensions } from "../redux/Actions";
 
 // COMPONENTS //
 import CircularProgressbar from 'react-circular-progressbar';
@@ -13,12 +13,15 @@ class Tree extends Component {
     super(props);
     this.state = {
       currentPageState: 0,
-      currentUnitState: 0
+      currentUnitState: 0,
+      mobileView: false,
     };
   }
 
   componentDidMount(){
-    this.setState({currentPageState: this.props.currentPage, currentUnitState: this.props.currentUnit})
+    this.setState({currentPageState: this.props.currentPage, currentUnitState: this.props.currentUnit});
+    window.addEventListener("resize", this.detectDimensions.bind(this));
+    this.detectDimensions();
   }
 
   componentDidUpdate(){
@@ -34,6 +37,7 @@ class Tree extends Component {
   addActivePage(plot){
     this.refs[plot].classList.toggle('activePage')
   }
+
   addActiveUnit(plot){
     this.refs[plot].classList.toggle('activeUnit')
   }
@@ -43,8 +47,16 @@ class Tree extends Component {
     this.props.setCurrentPage(page, unit.toString());
   }
 
+  detectDimensions() {
+    if(window.innerWidth < 1000) {
+      this.setState({mobileView: true})
+    } else {
+      this.setState({mobileView: false})
+    }
+  }
+
   render() {
-    
+    console.log(this.state.mobileView);
     return (
       <div className={'contentTree'}>
 
@@ -75,7 +87,7 @@ class Tree extends Component {
             
             <div ref={'hidden'+plot.id} className={'hiddenPages'}>
             { plot.content.map((plex) => { return (
-                <div tabIndex={'2'} onKeyDown={(event) => event.keyCode == 32 ? (this.pageChoice(plot.id, plex.id)):null}  onClick={() => {this.pageChoice(plot.id, plex.id)}} key={plex.id} className={'unitPage'}>
+                <div tabIndex={'2'} onKeyDown={(event) => event.keyCode == 32 ? (this.pageChoice(plot.id, plex.id), (this.state.mobileView ? this.props.detectDimensions(false) : null)) :null}  onClick={() => {this.pageChoice(plot.id, plex.id), (this.state.mobileView ? this.props.detectDimensions(false) : null)}} key={plex.id} className={'unitPage'}>
                   <a href={'#'}>{plex.title}</a>
                   { localStorage.getItem('nes-progress-'+ plot.id) ? localStorage.getItem('nes-progress-'+ plot.id).includes(plex.id) ? <svg id="checkmark" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 220.6 220.81"><title>icon-checkmark</title><g><path className="checkmarkBlue" d="M0,110.19C-.69,51.51,47.52-.35,110.93,0,173,.35,220.68,51.32,220.6,110.6c-.07,58.22-46.7,110.45-110.57,110.21C44.42,220.56-1,166.75,0,110.19Zm90.74,19.65L60.3,100.54l-16.39,17.6q23,23.05,47.8,47.94L177,78,159.43,61.12Z"/><path className="checkmarkWhite" d="M90.76,129.84l68.67-68.72L177,78,91.71,166.08q-24.79-24.89-47.8-47.94l16.39-17.6Z"/></g></svg>:null :null}
                 </div>
@@ -105,6 +117,7 @@ const mapDispatchToProps = dispatch => {
   return {
     setCurrentUnit: (value) => dispatch(setCurrentUnit(value)),
     setCurrentPage: (value, unit) => dispatch(setCurrentPage(value, unit)),
+    detectDimensions: (bool) => dispatch(detectDimensions(bool)),
   };
 };
 
