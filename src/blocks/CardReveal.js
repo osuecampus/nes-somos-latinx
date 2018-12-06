e33cb66ce5e6ac2deaf3c1b502bd3408fe1992ae
Austin Spaeth <austinspaeth@msn.com>
2018-12-05 20:15:31 -0800

    fix(CardReveal): Added spacing between cards when three are shown (adjusted padding and calculated w
    fix #92

--- src/blocks/CardReveal.js
+++ src/blocks/CardReveal.js
@@ -27,12 +27,12 @@ export default class CardReveal extends Component {
         {this.props.details.cards.map((block) => 
           <div key={block.id} className={css(ss.cardHolder)}>
             <div tabIndex={'4'} onKeyDown={(event) => event.keyCode == 32 ? this.cardReveal('card'+block.id, 'reveal'+block.id) : null } onClick={() => this.cardReveal('card'+block.id, 'reveal'+block.id)} ref={'card'+block.id} className={css(ss.card)}>
-              {block.revealText}
+              <p className={css(ss.cardText)}>{block.revealText}</p>
             </div>
             <div ref={'reveal'+block.id} className={css(ss.reveal)}>
-              {block.text ? <div className={css(ss.quoteBox)}>{block.text}</div> : null }
-              {block.textTwo ? <div className={css(ss.quoteBox)}>{block.textTwo}</div> : null }
-              {block.textThree ? <div className={css(ss.quoteBox)}>{block.textThree}</div> : null }
+              {block.text ? <div className={css(ss.cardText)}>{block.text}</div> : null }
+              {block.textTwo ? <div className={css(ss.cardText)}>{block.textTwo}</div> : null }
+              {block.textThree ? <div className={css(ss.cardText)}>{block.textThree}</div> : null }
             </div>
           </div>
         )}
@@ -58,7 +58,6 @@ const ss = StyleSheet.create({
       }
   },
   cardHolder:{
-
     flex:1,
     display:'flex',
     justifyContent:'center',
@@ -67,11 +66,16 @@ const ss = StyleSheet.create({
       height:340
     }
   },
+  cardText:{
+    padding:20,
+    color:'#fff',
+    fontSize:'smaller'
+  },
   card:{
     position: 'absolute',
     backgroundColor: '#1f60e2',
-    height: 250,
-    width: 250,
+    height: 275,
+    width: 275,
     transform: 'rotateY(-0deg)',
     backfaceVisibility: 'hidden',
     border: '1px solid rgba(0,0,0,.15)',
@@ -83,7 +87,7 @@ const ss = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     fontFamily:'Crimson Text',
-    padding: 25,
+    padding: 0,
     transition: 'all .2s ease-in-out',
     ':hover': {
         border: '1px solid #1148b9',
@@ -107,7 +111,7 @@ const ss = StyleSheet.create({
     display: 'flex',
     justifyContent: 'space-around',
     alignItems: 'center',
-    padding: 25,
+    padding: 0,
     transition: 'all .2s ease-in-out',
   },
   cardFlip:{
